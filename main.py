b735c0ed604190fe286b1a65643451941f02fbd2
Ratul <ratul@cefalo.com>
2022-07-28 09:02:12 +0600

    improved cli exposure

--- main.py
+++ main.py
@@ -26,25 +26,24 @@ def to_datetime(timestring):
         second=int(timestring[12:14]),
     )
 
+
+def main(ftp_name: str):
     store = []
-    files = ftp.mlsd(spectron["PATH"])
+
+    cred_meta = get_credentials(ftp_name)
+    ftp = FTP(host=cred_meta["HOSTNAME"])
+    ftp.login(user=cred_meta["USERNAME"], passwd=cred_meta["PASSWORD"])
+    files = ftp.mlsd(path=cred_meta["PATH"])
     for file in files:
         name = "__root__" if file[0] == "/" else file[0]
-        timestring = file[1]["modify"]
-        timestamp = datetime(
-            year = int(timestring[:4]),
-            month = int(timestring[4:6]),
-            day = int(timestring[6:8]),
-            hour = int(timestring[8:10]),
-            minute = int(timestring[10:12]),
-            second = int(timestring[12:14]),
-        )
+        timestring = file[1]["modify"]  # example 202208062359
+        timestamp = to_datetime(timestring)
         store.append((name, timestamp))
     ftp.quit()
-    df = pd.DataFrame(store, columns=("filename", "time"))
-    df = df.set_index("time")
+    df = pd.DataFrame(store, columns=("filename", "time")).set_index("time")
 
-    print(df.sort_index(axis="columns", ascending=False))
+    print(df.sort_index(axis="index", ascending=False))
+    df.to_csv("op.DataFrame.csv")
 
 
 if __name__ == "__main__":
